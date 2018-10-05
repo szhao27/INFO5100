3a0ddf7b09f4f10e0eeb15ddcf5e929e04f772c5
shan zhao <zhao.sha@husky.neu.edu>
2018-10-05 10:34:34 -0700

    assignment3

---
+++ ASSIGNMENT3/extracredit/extracredit.java
@@ -0,0 +1,22 @@
+package extracredit;
+
+import java.util.regex.Pattern;
+public class extracredit{
+	
+	static String reverse(String s) {
+		Pattern pattern= Pattern.compile("\\s");
+		String[] a=pattern.split(s);
+		String re="";
+		for(int i=0;i<a.length;i++) {
+			if(i==a.length-1) {
+				re=a[i]+re;}
+			else re=" "+a[i]+re;
+		}
+		return re;
+		
+ 	}
+	public static void main(String[] args) {
+		String a="The sky is blue";
+		System.out.println(reverse(a));
+	}
+}
--- ASSIGNMENT3/extracredit3/extracredit3.java
+++
@@ -1,9 +0,0 @@
-package extracredit3;
-
-public class extracredit3 {
-	public String reverse(String s) {
-		
-	}
-	
-
-}
